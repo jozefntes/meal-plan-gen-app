5708ab1f2a25a421b507d441cb6e0a123be1609c
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-01-24 22:25:49 -0600

    Init commit

---
+++ server/src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const cors = require("cors");
+
+PORT = 8080;
+
+// connect to db
+let db;
+(async () => {
+  db = await open({
+    filename: "db/data.sqlite",
+    driver: sqlite3.Database,
+  });
+})();
+
+app = express();
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
