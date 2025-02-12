42e0c2a93b8757fd9417f5939a53a08c9a8beebe
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-02-11 18:29:49 -0600

    Rename meal-item class to meal-card; add functionality for marking meals as done

--- client/src/components/MealCard.jsx
+++ client/src/components/MealCard.jsx
@@ -1,31 +1,39 @@
 import "./MealCard.css";
 
 export default function MealCard({
+  id,
   time,
   name,
   imageURL,
   protein,
   carbs,
   fat,
+  done,
+  onMealDone,
 }) {
   const backgroundStyle = {
     background: `linear-gradient(rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 50%), linear-gradient(45deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 50%),
         url(${imageURL})`,
-    backgroundPosition: `center`,
-    backgroundRepeat: `no-repeat`,
-    backgroundSize: `cover`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
   };
 
   return (
     <>
-      <li className="meal-item" style={backgroundStyle}>
+      <li className="meal-card" style={backgroundStyle}>
         <div className="meal-header">
-          <img src="../icons/check.svg" />
           <div className="meal-info">
             <p className="body-s">{time}</p>
             <p className="body-s">{name}</p>
           </div>
-          <img src="../icons/heart.svg" />
+          <button className="add-btn" onClick={() => onMealDone(id)}>
+            {done ? (
+              <img src="../icons/check.svg" />
+            ) : (
+              <img src="../icons/plus.svg" />
+            )}
+          </button>
         </div>
         <div className="meal-footer">
           <p className="body-s">{protein} g protein</p>
