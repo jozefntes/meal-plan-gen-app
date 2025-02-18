1273af892f0b7c4cf0f1f13160240be880726372
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-02-18 15:16:29 -0600

    Add routing for Create component in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react";
 import page from "page";
 
 import Home from "./Home";
+import Create from "./Create";
 
 function App() {
   const [route, setRoute] = useState("home");
 
   useEffect(() => {
     page("/", () => setRoute("home"));
+    page("/create", () => setRoute("create"));
     page.start();
   }, []);
 
-  return <>{route === "home" && <Home />}</>;
+  return (
+    <>
+      {route === "home" && <Home />}
+      {route === "create" && <Create />}
+    </>
+  );
 }
 
 export default App;
