5768a9d42ee2df324ee4824e1fbbfda93bf69718
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-02-23 17:35:19 -0600

    Refactor meal group component and styles for better modularity and maintainability

---
+++ client/src/components/MealGroup.jsx
@@ -0,0 +1,30 @@
+import "./MealGroup.css";
+
+export default function MealGroup({ title, recipes, onCreateNew }) {
+  return (
+    <div className="meal-group">
+      <h6>{title}</h6>
+
+      <div className="picker">
+        <div className="create-new-btn" onClick={onCreateNew}>
+          <div>
+            <img
+              className="create-icon"
+              src="/icons/circle-dashed-plus.svg"
+              alt="Create New"
+            />
+          </div>
+          <p className="body-s">+ Create New</p>
+        </div>
+        <ul>
+          {recipes.map((recipe) => (
+            <li className="picker__item" key={recipe.id}>
+              <img src={recipe.imageURL} alt={recipe.title} />
+              <p className="body-s">{recipe.title}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
