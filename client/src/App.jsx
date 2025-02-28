9854b76dc833db9d1c42be5dfcd3e4840c9fba94
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-02-27 18:34:11 -0600

    Add SignIn and SignUp routes and components to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import page from "page";
 
 import Home from "./Home";
 import MealPlanGenerator from "./MealPlanGenerator";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 function App() {
   const [route, setRoute] = useState("home");
@@ -10,6 +12,8 @@ function App() {
   useEffect(() => {
     page("/", () => setRoute("home"));
     page("/generate", () => setRoute("generate"));
+    page("/signin", () => setRoute("signin"));
+    page("/register", () => setRoute("register"));
     page.start();
   }, []);
 
@@ -17,6 +21,8 @@ function App() {
     <>
       {route === "home" && <Home />}
       {route === "generate" && <MealPlanGenerator />}
+      {route === "signin" && <SignIn />}
+      {route === "register" && <SignUp />}
     </>
   );
 }
