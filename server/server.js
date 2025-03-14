941e6acbcecf4d854a5067710fc3ead1da55bb2b
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-03-14 14:33:53 -0500

    Add recipe ID to generated recipe response

--- server/server.js
+++ server/server.js
@@ -433,6 +433,7 @@ app.post("/api/generate_recipe", verifyToken, async (req, res) => {
     // Save recipe to database
     try {
       const docRef = await db.collection("recipes").add(recipe);
+      recipe.id = docRef.id;
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
