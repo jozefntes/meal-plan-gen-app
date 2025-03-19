1131b78b91f5c09c1dbf77d6ea4391a26c44ac92
jozefntes <123389422+jozefntes@users.noreply.github.com>
2025-03-19 15:44:39 -0500

    Add MealCard component integration and conditional rendering in MealGroup

--- client/src/components/MealGroup.jsx
+++ client/src/components/MealGroup.jsx
@@ -1,3 +1,4 @@
+import MealCard from "./MealCard";
 import "./MealGroup.css";
 import CircledPlusIcon from "../icons/CircledPlusIcon";
 
@@ -6,37 +7,65 @@ export default function MealGroup({
   recipes,
   onCreateNew,
   onSelectRecipe,
+  applicationContext,
+  onDeleteRecipe,
 }) {
   return (
     <div className="meal-group">
       <h6>{title}</h6>
 
       <div className="picker">
-        <div className="create-new-btn" onClick={onCreateNew}>
-          <div>
-            <CircledPlusIcon size={80} />
+        {applicationContext !== "recipes" && (
+          <div className="create-new-btn" onClick={onCreateNew}>
+            <div>
+              <CircledPlusIcon size={80} />
+            </div>
+            <p className="body-s">+ Create New</p>
           </div>
-          <p className="body-s">+ Create New</p>
-        </div>
+        )}
         <ul>
-          {recipes.length > 0 &&
+          {recipes.length > 0 ? (
             recipes.map((recipe) => (
               <li className="picker__item" key={recipe.id}>
-                <input
-                  type="checkbox"
-                  className="hidden-checkbox"
-                  id={`recipe-${recipe.id}`}
-                  onChange={(e) => onSelectRecipe(recipe.id, e.target.checked)}
-                />
-                <label className="recipe-label" htmlFor={`recipe-${recipe.id}`}>
-                  <img
-                    src={`data:image/png;base64,${recipe.image}`}
-                    alt={recipe.title}
+                {applicationContext === "recipes" ? (
+                  <MealCard
+                    id={recipe.id}
+                    mealGroup={recipe.mealGroup}
+                    title={recipe.title}
+                    image={recipe.image}
+                    nutrition={recipe.nutrition}
+                    done={recipe.done}
+                    onMealDone={onSelectRecipe}
+                    applicationContext={applicationContext}
+                    onDeleteRecipe={onDeleteRecipe}
                   />
-                  <p className="body-s">{recipe.title}</p>
-                </label>
+                ) : (
+                  <>
+                    <input
+                      type="checkbox"
+                      className="hidden-checkbox"
+                      id={`recipe-${recipe.id}`}
+                      onChange={(e) =>
+                        onSelectRecipe(recipe.id, e.target.checked)
+                      }
+                    />
+                    <label
+                      className="recipe-label"
+                      htmlFor={`recipe-${recipe.id}`}
+                    >
+                      <img
+                        src={`data:image/png;base64,${recipe.image}`}
+                        alt={recipe.title}
+                      />
+                      <p className="body-s">{recipe.title}</p>
+                    </label>
+                  </>
+                )}
               </li>
-            ))}
+            ))
+          ) : (
+            <p className="body-m">No recipes found</p>
+          )}
         </ul>
       </div>
     </div>
